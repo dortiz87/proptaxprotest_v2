62f62c88d941a4c378ba0f9b827b856f6f8549c2
dortiz87 <daniel@iortiz.com>
2018-11-06 09:36:07 -0600

    Add project files.

---
+++ proptaxprotest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace proptaxprotest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
